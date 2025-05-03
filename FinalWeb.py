39dbfc9027d48e64a83bb6575c315c28dfc0971c
hannahmgordon <hmg226@lehigh.edu>
2025-05-02 21:16:35 -0400

    DONE

--- FinalWeb.py
+++ FinalWeb.py
@@ -206,6 +206,7 @@ Our original hypothesis that similar disclosures are associated with stronger lo
     # Final images
     st.image("pics/pic1.png", use_container_width=True)
     st.image("pics/pic2.png", use_container_width=True)
+    st.image("pics/pic3.png", use_container_width=True)
 
 # ---------- Page: Our Team ----------
 # ---------- Page: Our Team ----------
